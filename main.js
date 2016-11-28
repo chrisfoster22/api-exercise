7aaed592308cc4a877a61400d9d6f47655872adb
Chris Foster <fosterchris22@gmail.com>
2016-11-28 12:10:07 -0500

    Create jquery example for appending.

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ $(document).ready(function() {
 	var coffee = request.searchKeyword("coffee");
 
 	for (var i=0; i < coffee.length; i++) {
-		$('#content').html() += coffee[i].eateryName;
+		var titleDiv = $("<div></div>");
+		titleDiv.addClass('listing');
+		titleDiv.append('<div>' +coffee[i].eateryName + '</div>');
+		$('#content').append(titleDiv);
 	}
 
 })
